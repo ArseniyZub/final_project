6ace751a3510802fa71baed75abf83c9e956e10a
Arseniy <zubkov_000@list.ru>
2024-06-10 20:28:40 +0300

    2

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import *
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.urls import reverse
+from django.urls import *
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100,)
